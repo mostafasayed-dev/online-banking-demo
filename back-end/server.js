49fd38694597419ca41c601139627981b730ba70
mostafasayed-dev <mostafasayed.fathi@gmail.com>
2021-02-01 00:47:03 +0200

    testing deployment

--- back-end/server.js
+++ back-end/server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log('server is up and running on port ' + port)
+    console.log('server is up and running on port ' + port + '.')
 })
